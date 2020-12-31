5ab6272c72dcf249c6cffd7d9cffd2ef9779eb91
binji1 <binji@gmail.com>
2020-12-31 22:36:52 +0800

    topological sort

---
+++ BFS/LintCode127.java
@@ -0,0 +1,70 @@
+/**
+ * Definition for Directed graph.
+ * class DirectedGraphNode {
+ *     int label;
+ *     ArrayList<DirectedGraphNode> neighbors;
+ *     DirectedGraphNode(int x) { label = x; neighbors = new ArrayList<DirectedGraphNode>(); }
+ * };
+ */
+
+public class Solution {
+    /*
+     * @param graph: A list of Directed graph node
+     * @return: Any topological order for the given graph.
+     */
+    public ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
+        // write your code here
+        if(graph == null || graph.size() == 0) {
+            return null;
+        }
+        HashMap<DirectedGraphNode, Integer> map = new HashMap<>();
+        // get indegree of graph
+        getInDegree(graph, map);
+        ArrayList<DirectedGraphNode> startNodes = getStartNodes(map);
+        // get topological sort order by bfs
+        ArrayList<DirectedGraphNode> order = bfs(startNodes, map);
+        return order.size() == graph.size()? order: null;
+    }
+    
+    private void getInDegree(ArrayList<DirectedGraphNode> graph, HashMap<DirectedGraphNode, Integer> map) {
+        // initialize map for every node in graph
+        for(DirectedGraphNode node: graph) {
+            map.put(node, 0);
+        }
+        for(DirectedGraphNode node: graph) {
+            for(DirectedGraphNode neighbor: node.neighbors) {
+                map.put(neighbor, map.get(neighbor) + 1);
+            }
+        }
+    }
+    
+    private ArrayList<DirectedGraphNode> getStartNodes(HashMap<DirectedGraphNode, Integer> map) {
+        ArrayList<DirectedGraphNode> startNodes = new ArrayList<>();
+        for(DirectedGraphNode temp: map.keySet()) {
+            if(map.get(temp) == 0) {
+                startNodes.add(temp);
+            }
+        }
+        return startNodes;
+    }
+    
+    private ArrayList<DirectedGraphNode> bfs(ArrayList<DirectedGraphNode> startNodes,
+    HashMap<DirectedGraphNode, Integer> map) {
+        Queue<DirectedGraphNode> que = new LinkedList<DirectedGraphNode>();
+        ArrayList<DirectedGraphNode> res = new ArrayList<>();
+        for(DirectedGraphNode startNode: startNodes) {
+            que.offer(startNode);
+        }
+        while(!que.isEmpty()) {
+            DirectedGraphNode temp = que.poll();
+            res.add(temp);
+            for(DirectedGraphNode neighbor: temp.neighbors) {
+                map.put(neighbor, map.get(neighbor) - 1);
+                if(map.get(neighbor) == 0) {
+                    que.offer(neighbor);
+                }
+            }
+        }
+        return res;
+    }
+}
--- README.md
+++ README.md
@@ -7,3 +7,9 @@ LintCode 178 Graph Valid Tree
 LeetCode 133 Clone Graph
     (1) use Map to track the relation between old and new node
     (2) add neighbors to new node
+
+
+LintCode 127 Topological Sorting
+    (1) get indegree map of graph
+    (2) get startNodes of topological sort
+    (3) use bfs get full order
