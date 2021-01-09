43d31c6860c245bd904d403f34277a949a28a327
binji1 <binji@gmail.com>
2021-01-09 23:00:51 +0800

    add some comments

--- BFS/Lintcode178.java
+++ BFS/Lintcode178.java
@@ -9,15 +9,17 @@ public class Solution {
         if(edges == null || edges.length == 0) {
             return n == 1;
         }
+        // 判断节点数 = 边数 + 1
         if(n -1 != edges.length) {
             return false;
         }
+        // 如果满足上述条件，但是不是树，必定有环，
+        // 同时无法从一个节点出发，到达其他所有节点
         HashMap<Integer, HashSet<Integer>> map = new HashMap<Integer, HashSet<Integer>>();
         HashSet<Integer> visited = new HashSet<>();
         Queue<Integer> que = new LinkedList<Integer>();
         que.offer(0);
         visited.add(0);
-        // create map for edges
         for(int[] edge : edges) {
             if(!map.containsKey(edge[0])) {
                 map.put(edge[0], new HashSet<Integer>());
@@ -28,7 +30,6 @@ public class Solution {
             }
              map.get(edge[1]).add(edge[0]);
         }
-        
         while(!que.isEmpty()) {
             int value = que.poll();
             for(int temp : map.get(value)) {
@@ -38,6 +39,7 @@ public class Solution {
                 }
             }
         }
+        // 判断是否遍历了所有的节点
         return visited.size() == n;
     }
 }
